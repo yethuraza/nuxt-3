7d16d37e87f502c3c240872c5c8bc03a0c5d6782
YTR <151596674+yethuraza@users.noreply.github.com>
2024-06-19 14:00:20 +0630

    nuxt 3 setup, routing

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    Page: Home
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/movies.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    Page: movies
+  </div>
+</template>
+
+<style scoped></style>
