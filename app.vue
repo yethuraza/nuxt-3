c9f849ef0e87b40f02fe0c456c1383404e6bb699
YTR <151596674+yethuraza@users.noreply.github.com>
2024-12-11 09:07:21 +0630

    add:tailwind

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
-    <nav>
-      <NuxtLink to="/">Home</NuxtLink> |
-      <NuxtLink to="/movies">Movies</NuxtLink>
-    </nav>
-    <NuxtPage/>
+  <div class="w-screen h-screen flex justify-center items-center">
+    <div class="w-[500px] h-[400px] bg-slate-300">
+      <nav>
+        <NuxtLink to="/">Home</NuxtLink> |
+        <NuxtLink to="/movies">Movies</NuxtLink>
+      </nav>
+      <NuxtPage />
+    </div>
   </div>
 </template>
--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -2,6 +2,15 @@
     const route = useRoute()
     const {data} = useAsyncData(`/movies/${route.params.id}`,() => {
         return $fetch(`http://www.omdbapi.com/?apikey=a2e4475f&i=${route.params.id}`)
+    }, {
+        pick: ['Plot','Title'],
+        // transform(data){
+        //     return {
+        //         Plot: data.Plot,
+        //         Title: data.Title
+        //     }
+            
+        // }
     })
     
 </script>
